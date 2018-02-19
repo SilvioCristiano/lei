9db85fdaeb1cdad84ab9834e4b1a2bfa1df9d687
SilvioCristiano <sivuca1@gmail.com>
2018-02-19 03:14:31 -0300

    recursos e service

---
+++ src/main/java/com/silvio/cursomc/service/ClienteService.java
@@ -0,0 +1,27 @@
+package com.silvio.cursomc.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silvio.cursomc.domain.Cliente;
+import com.silvio.cursomc.repositories.ClienteRepository;
+
+import com.silvio.cursomc.service.exceptions.ObjectNotFoundExcepion;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+
+		Cliente obj = repo.findOne(id);
+		
+		if(obj==null) {
+			throw new ObjectNotFoundExcepion("Objeto não encontrado! Id " + id + " Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
+}
