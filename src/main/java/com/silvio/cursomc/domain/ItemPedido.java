fbf4b7128591e309dc007572691dc9b5e38adb75
SilvioCristiano <sivuca1@gmail.com>
2018-03-20 00:54:33 -0300

    subtotal e total do pedido

--- src/main/java/com/silvio/cursomc/domain/ItemPedido.java
+++ src/main/java/com/silvio/cursomc/domain/ItemPedido.java
@@ -30,6 +30,10 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade; 
+		
+	}
 	
 	public ItemPedidoPK getId() {
 		return id;
