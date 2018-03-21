fc54fd58a09b2ef1273daee4ce95e69b4606cccc
SilvioCristiano <sivuca1@gmail.com>
2018-03-21 18:17:50 -0300

    Criando profile de dev

---
+++ src/main/java/com/silvio/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.silvio.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.silvio.cursomc.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl.auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.InstantiateTestDatabase();
+		
+		return true;
+	}
+}
