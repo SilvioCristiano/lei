59e50e19c87bb1c2d77fb2d7d920846e9fca859b
SilvioCristiano <sivuca1@gmail.com>
2018-04-16 03:35:12 -0300

    prod5

--- src/main/java/com/silvio/cursomc/service/EmailService.java
+++ src/main/java/com/silvio/cursomc/service/EmailService.java
@@ -2,12 +2,13 @@ package com.silvio.cursomc.service;
 
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.silvio.cursomc.domain.Cliente;
 import com.silvio.cursomc.domain.Pedido;
 
 
-
+@Service
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
