dd909bfb79e2cc94cf9c851babaf9c3f25e6c2f5
SilvioCristiano <sivuca1@gmail.com>
2018-04-16 22:56:54 -0300

    Producao

--- src/main/java/com/silvio/cursomc/service/EmailService.java
+++ src/main/java/com/silvio/cursomc/service/EmailService.java
@@ -2,13 +2,12 @@ package com.silvio.cursomc.service;
 
 
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
 
 import com.silvio.cursomc.domain.Cliente;
 import com.silvio.cursomc.domain.Pedido;
 
 
-@Service
+
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
--- src/main/java/com/silvio/cursomc/service/SmtpEmailService.java
+++ src/main/java/com/silvio/cursomc/service/SmtpEmailService.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+@Service
 public class SmtpEmailService extends AbstractEmailService {
 
 	@Autowired
